1d7eaac45843556e43387e5246def8617be96357
Alberto García Hierro <alberto@garciahierro.com>
2013-10-31 21:13:34 +0100

    Move quantum related macros to quantum.h

--- pixel.c
+++ pixel.c
@@ -1,9 +1,6 @@
 #include <magick/api.h>
 
-#ifndef ScaleQuantumToChar
-#define ScaleQuantumToChar(x) (((unsigned char)((x)/(QuantumRange/255.0))))
-#define ScaleCharToQuantum(x) (((Quantum)((x)/(QuantumRange/255.0))))
-#endif
+#include "quantum.h"
 
 unsigned char
 quantum_to_char(Quantum q)
---
+++ quantum.h
@@ -0,0 +1,11 @@
+#ifndef QUANTUM_H
+#define QUANTUM_H
+
+#include <magick/api.h>
+
+#ifndef ScaleQuantumToChar
+#define ScaleQuantumToChar(x) (((unsigned char)((x)/(QuantumRange/255.0))))
+#define ScaleCharToQuantum(x) (((Quantum)((x)/(QuantumRange/255.0))))
+#endif
+
+#endif
