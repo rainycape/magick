162924dad528b4b475150405038f507876e1634d
Alberto García Hierro <alberto@garciahierro.com>
2014-03-26 20:40:15 +0000

    Return an error if CompositeImage fails

--- composite.c
+++ composite.c
@@ -4,6 +4,8 @@ Image * compositeImage(Image *canvas, void *data, ExceptionInfo *ex)
 {
     CompositeData *d = data;
     if (!CompositeImage(canvas, d->composite, d->draw, d->x, d->y)) {
+        ex->severity = DrawError;
+        return NULL;
     }
     return canvas;
 }
