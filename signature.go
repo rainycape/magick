9fd61ef1c1617e5c1c598fecbdd134dfc41d94d3
Alberto García Hierro <alberto@garciahierro.com>
2014-03-27 20:41:49 +0000

    Change Signature() to return the actual signature
    It was previously returning success/failure, user had to then
    grab the signature from the image properties. API is more convenient
    this way, since the user can detect a failure when the returned
    signature is empty.

--- signature.go
+++ signature.go
@@ -7,6 +7,9 @@ import "C"
 // implementation of the NIST SHA-256 Message Digest algorithm. This
 // signature uniquely identifies the image and is convenient for determining
 // if an image has been modified or whether two images are identical.
-func (im *Image) Signature() uint {
-	return uint(C.SignatureImage(im.image))
+func (im *Image) Signature() string {
+	if !im.HasProperty("signature") {
+		C.SignatureImage(im.image)
+	}
+	return im.Property("signature")
 }
