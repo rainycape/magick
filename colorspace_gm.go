1d9955b90c09c33c65b41ea721b5523c3d2d13bd
Alberto García Hierro <alberto@garciahierro.com>
2014-06-21 19:56:35 +0100

    Implement color space trasformation in GM

--- colorspace_gm.go
+++ colorspace_gm.go
@@ -2,6 +2,16 @@
 
 package magick
 
+// #include <magick/api.h>
+// #include "bridge.h"
+//
+// int
+// transformImageColorspace(Image *image, void *cs, ExceptionInfo *ex) {
+//  ColorspaceType *c = cs;
+//  return TransformColorspace(image, *c);
+// }
+import "C"
+
 func (im *Image) transformColorspace(cs Colorspace) (*Image, error) {
-	return nil, notImplementedError("TransformColorspace")
+	return im.applyDataFunc("transforming-colorspace", C.ImageDataFunc(C.transformImageColorspace), &cs)
 }
